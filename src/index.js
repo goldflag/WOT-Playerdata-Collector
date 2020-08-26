4adf0ebe740f40371b4b0887c000b58008e2ab13
Goldflag <bill2000yang@gmail.com>
2020-08-26 02:40:30 -0400

    hi

--- src/index.js
+++ src/index.js
@@ -6,36 +6,55 @@ const tankNames = require("./data/tankNames");
 const WN8calculator = require("./wn8");
 const calculatePlayerWN8 = require("./calculatePlayerWN8");
 
-async function insertCurves(id, WR, overallWR, WN8, overallWN8) {
+async function insertCurves(id, WR, overallWR, WN8, overallWN8, battles) {
     overallWR = parseInt(overallWR+0.5);
     overallWN8 = Math.floor((overallWN8 + 50) / 100) * 100;
-    //console.log(WR);
     if (overallWR >= 42 && overallWR <= 65 && overallWN8 >= 200 && overallWN8 <= 3500) {
-        await db.query(`UPDATE NAcurves SET WR${overallWR} = array_append(WR${overallWR}, ${WR}::REAL), WN${overallWN8} = array_append(WN${overallWN8}, ${parseInt(WN8)}) WHERE tank_id = ${id}`);
+        await db.query(`UPDATE NAcurves SET WR${overallWR} = WR${overallWR} + ${WR*battles}, WN${overallWN8} = WN${overallWN8} + ${parseInt(WN8*battles)} WHERE tank_id = ${id}`);
+        await db.query(`UPDATE NAcurvesCount SET WR${overallWR} = WR${overallWR} + ${battles}, WN${overallWN8} = WN${overallWN8} + ${battles} WHERE tank_id = ${id}`);
     }
 }
 
 async function initCurves() {
     const keys = Object.keys(tankNames);
     for (const key of keys) {
-        console.log('key' + key);
-
-        const arr = [];
-        const arr2 = [];
-        await db.query(`INSERT INTO NAcurves (
-            tank_id, name, 
-            WR42, WR43, WR44, WR45, WR46, WR47, WR48, WR49, WR50, WR51, WR52, WR53,
-            WR54, WR55, WR56, WR57, WR58, WR59, WR60, WR61, WR62, WR63, WR64, WR65,
-            WN200, WN300, WN400, WN500, WN600, WN700, WN800, WN900, WN1000, WN1100, 
-            WN1200, WN1300, WN1400, WN1500, WN1600, WN1700, WN1800, WN1900, WN2000, WN2100, 
-            WN2200, WN2300, WN2400, WN2500, WN2600, WN2700, WN2800, WN2900, WN3000, WN3100, 
-            WN3200, WN3300, WN3400, WN3500
-        ) VALUES (
-            $1, $2, 
-            $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3,
-            $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4,
-            $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4
-        )`, [key, tankNames[key].short_name, arr, arr2]);
+        if (tankNames[key].tier >= 5) {
+            console.log('key' + key);
+            await db.query(`INSERT INTO NAcurves (
+                tank_id, name, 
+                WR42, WR43, WR44, WR45, WR46, WR47, WR48, WR49, WR50, WR51, WR52, WR53,
+                WR54, WR55, WR56, WR57, WR58, WR59, WR60, WR61, WR62, WR63, WR64, WR65,
+                WN200, WN300, WN400, WN500, WN600, WN700, WN800, WN900, WN1000, WN1100, 
+                WN1200, WN1300, WN1400, WN1500, WN1600, WN1700, WN1800, WN1900, WN2000, WN2100, 
+                WN2200, WN2300, WN2400, WN2500, WN2600, WN2700, WN2800, WN2900, WN3000, WN3100, 
+                WN3200, WN3300, WN3400, WN3500
+                ) VALUES (
+                    $1, $2, 
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                )`, [key, tankNames[key].short_name]);
+            await db.query(`INSERT INTO NAcurvesCount (
+                tank_id, name, 
+                WR42, WR43, WR44, WR45, WR46, WR47, WR48, WR49, WR50, WR51, WR52, WR53,
+                WR54, WR55, WR56, WR57, WR58, WR59, WR60, WR61, WR62, WR63, WR64, WR65,
+                WN200, WN300, WN400, WN500, WN600, WN700, WN800, WN900, WN1000, WN1100, 
+                WN1200, WN1300, WN1400, WN1500, WN1600, WN1700, WN1800, WN1900, WN2000, WN2100, 
+                WN2200, WN2300, WN2400, WN2500, WN2600, WN2700, WN2800, WN2900, WN3000, WN3100, 
+                WN3200, WN3300, WN3400, WN3500
+                ) VALUES (
+                    $1, $2, 
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                )`, [key, tankNames[key].short_name]);
+            // ) VALUES (
+            //     $1, $2, 
+            //     $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3, $3,
+            //     $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4,
+            //     $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4, $4
+            // )`, [key, tankNames[key].short_name, arr, arr2]);
+        }
     }
 }
 
@@ -85,7 +104,6 @@ async function initTable() {
 
                 DPG,
                 WN8s
-
             ) 
             VALUES ( $1, $2, $3, $4, $5, $6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, $7, $8)`, 
             [key, tankNames[key].short_name, tankNames[key].tier, tankNames[key].type, tankNames[key].nation, tankNames[key].is_premium, arr, arr2]);
@@ -115,6 +133,30 @@ async function APIcall(i, index) {
             const playerWN8 = parseInt(calculatePlayerWN8(tankData));
             const playerWinrate = data4.data[`${i}`].statistics.all.wins*100 / data4.data[`${i}`].statistics.all.battles;
 
+            // CREATE TABLE naOverallstats (
+            //     id serial NOT NULL,
+            //     player_id INTEGER NOT NULL PRIMARY KEY, 
+            //     username VARCHAR(24) NOT NULL, 
+            //     WN8 INTEGER NOT NULL,
+            //     Winrate REAL NOT NULL, 
+            //     battles INTEGER NOT NULL,
+            //     creationtime INTEGER NOT NULL
+            // );
+
+            await db.query(
+                `INSERT INTO naOverallstats (
+                    player_id, 
+                    username, 
+                    WN8,
+                    Winrate, 
+                    battles,
+                    creationtime
+                ) VALUES 
+                (
+                    $1, $2, $3, $4, $5, $6
+                )
+                `,
+                [i, data4.data[`${i}`].nickname, playerWN8, playerWinrate, data4.data[`${i}`].statistics.all.battles, data4.data[`${i}`].created_at]);
 
             tankData.map( async (row, index) => {
                 if (row.tank_id in tankNames) {
@@ -157,7 +199,9 @@ async function APIcall(i, index) {
                     else if (tankNames[row.tank_id].tier >= 8) {
                         if (row.all.battles >= 75) db.query(`UPDATE tankdatana SET DPG = array_append(DPG, ${parseInt(avgDamage)}), WN8s = array_append(WN8s, ${WN8}) WHERE tank_id = ${row.tank_id}`);
                     }
-                    insertCurves(row.tank_id, winrate.toFixed(2), playerWinrate, WN8, playerWN8);
+                    if (tankNames[row.tank_id].tier >= 5) {
+                        insertCurves(row.tank_id, winrate.toFixed(2), playerWinrate, WN8, playerWN8, row.all.battles);
+                    }
                 }
             });
         }
@@ -169,7 +213,7 @@ async function APIcall(i, index) {
 
 function loop() {
     let counter = 1000000000;
-    for (let i = 0; i < 1000; ++i) {
+    for (let i = 0; i < 250; ++i) {
         setTimeout(function () {
             //const id = counter + 40*i;
             const id = counter + Math.floor(Math.random() * 40000000); 
